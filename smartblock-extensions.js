cc33a581f5ebf1b87f5aa7fc6e8ecc2922e6db00
Jon Baum <Ryxai@users.noreply.github.com>
2021-12-14 00:12:36 -0500

    Fixing to most generic variation

--- smartblock-extensions.js
+++ smartblock-extensions.js
@@ -2,11 +2,8 @@ window.roamjs.extension.smartblocks.registerCommand({
    text: 'UPDATEPAGE',
    help: "Updates the page title referenced by the first argument with the text in the rest.\n\n1. Page reference\n\n2. Text to update with",
   handler: (ref = "", ...rest) => {
-      const normRef = smartBlocksContext.variables[ref] || ref;
       const text = rest.join(",");
-      const normOut = smartBlocksContext.variables[text] || text;
-      const uid = extractRef(normRef);
-      window.roamAlphaAPI.updatePage({"page":{"uid": uid,"string": normOut}});
+      window.roamAlphaAPI.updatePage({"page":{"uid": uid,"string": text}});
       return "";
   }
 });
