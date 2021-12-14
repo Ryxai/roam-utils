e963ac047e076d43dcb3cf031eadcb91af483e02
Jon Baum <Ryxai@users.noreply.github.com>
2021-12-14 18:40:51 -0500

    Fixed updatetitle

--- ext_min.js
+++ ext_min.js
@@ -33,7 +33,7 @@ window.sm_ext.vars = {};
 regCommand({
    text: 'UPDATEPAGETITLE',
    help : "Updates the page title referenced by the first argument with the text in the rest.\n\n1. Page reference\n\n2. Text to update with",
-   handler: () => (ref, ...rest) => {
+   handler: () => (uid, ...rest) => {
       const text = rest.join(",");
       window.roamAlphaAPI.updatePage({page:{title: text, uid: uid}});
       return "";
