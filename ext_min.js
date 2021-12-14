cdb7614bf82831e412b75b947728c46432a7addd
Jon Baum <Ryxai@users.noreply.github.com>
2021-12-14 16:22:30 -0500

    Tried a new approach with iterative registration

--- ext_min.js
+++ ext_min.js
@@ -1,4 +1,35 @@
-window.roamjs.extension.smartblocks.registerCommand({
+regCommand = (e) => {
+  const text = e.text.toUpperCase();
+  const help = e.help;
+  const handler = e.handler;
+  const register = (retry) =>
+    window.roamjs?.extension?.smartblocks?.registerCommand
+      ? window.roamjs.extension.smartblocks.registerCommand({
+          text,
+          jelp,
+          handler
+        })
+      : retry === 120 && window.roamjs
+      ? !(window.roamjs = {
+          ...window.roamjs,
+          extension: {
+            ...window.roamjs.extension,
+            [text]: {
+              ...window.roamjs.extension[text],
+              registerSmartBlocksCommand: () => {
+                window.roamjs?.extension.smartblocks.registerCommand({
+                  text,
+                  handler,
+                });
+              },
+            },
+          },
+        })
+      : window.setTimeout(() => register(retry + 1), 1000);
+  register(0);
+};
+
+regCommand({
    text: 'UPDATEPAGETITLE',
    help : "Updates the page title referenced by the first argument with the text in the rest.\n\n1. Page reference\n\n2. Text to update with",
    handler: () => (ref: string, ...rest: string[]) => {
@@ -8,7 +39,7 @@ window.roamjs.extension.smartblocks.registerCommand({
       return "";
    }
 });
-window.roamjs.extension.smartblocks.registerCommand(
+regCommand(
 {
         text: "SETEXTVAR",
         help: "Returns a variable from the smt-ext environment\n\n1. Variable name",
@@ -17,14 +48,14 @@ window.roamjs.extension.smartblocks.registerCommand(
                 return "";
         }
 });
-window.roamjs.extension.smartblocks.registerCommand({
+regCommand({
         text: "GETEXTVAR",
         help: "Create a variable in memory in the smt-ext environment\n\n1. Variable name\n\n2: Value of variable",
         handler: () => (name: string) => {
                 return window.sm_ext.vars[name];
         }
 });
-window.roamjs.extension.smartblocks.registerCommand({
+regCommand({
   text: "CLEAREXTVAR",
   help: "Deletes a variable from the smt-ext environment\n\n1. Variable name",
   handler: () => (name : string) => {
@@ -32,7 +63,7 @@ window.roamjs.extension.smartblocks.registerCommand({
     return "";
   }
 });
-window.roamjs.extension.smartblocks.registerCommand({
+regCommand({
   text: "DELETEBLOCK",
   help: "Deletes a block\n\n1. Block uid",
   handler: () => (uid: string) => {
