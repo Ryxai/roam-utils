72e41c7d2b994d48880abff21e38cad2d97dedb3
Ryxai <jmichaelbaum@gmail.com>
2021-12-14 01:56:13 -0500

    Removed redundant script

--- smartblock-extensions.js
+++
@@ -1,9 +0,0 @@
-window.roamjs.extension.smartblocks.registerCommand({
-   text: 'UPDATEPAGE',
-   help: "Updates the page title referenced by the first argument with the text in the rest.\n\n1. Page reference\n\n2. Text to update with",
-  handler: (ref = "", ...rest) => {
-      const text = rest.join(",");
-      window.roamAlphaAPI.updatePage({"page":{"uid": uid,"string": text}});
-      return "";
-  }
-});
