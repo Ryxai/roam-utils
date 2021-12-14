d02032576bfc02e23b3c35dab0e1407176716e76
Ryxai <jmichaelbaum@gmail.com>
2021-12-14 03:48:53 -0500

    update

--- smartblock-extensions.js
+++
@@ -1,9 +0,0 @@
-registerSmartBlocksCommand({
-   text: 'UPDATEPAGETITLE',
-   help: "Updates the page title referenced by the first argument with the text in the rest.\n\n1. Page reference\n\n2. Text to update with",
-  handler: (ref = "", ...rest) => {
-      const text = rest.join(",");
-      window.roamAlphaAPI.updatePage({page:{title: text, uid: ref}});
-      return "";
-  }
-});
